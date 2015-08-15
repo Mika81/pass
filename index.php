8f74b67f883485ba4b2baa7ea73e782776f1608e
Mika <webdeveloper@diw.fr>
2015-08-15 19:19:33 +0200

    Creates index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'src/functions/functions.php';
+
+require_once 'src/classes/Password.class.php';
+
+include_once 'src/functions/create_password.php';
+
+print $lenght;
+
+
+include_once 'views/home.html';
+
+
